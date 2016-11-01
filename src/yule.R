8ae76781a2d3ee4fb168595363512740bd4cf555
Pedro Pinto da Silva (PGR) <ppintodasilva@gmail.com>
2016-11-01 12:32:26 +0000

    New branch test

---
+++ src/yule.R
@@ -0,0 +1,12 @@
+
+
+giveMeAYule = function(lambda, n, leafNames) {
+  
+  
+  yule = data.frame(Name   = as.character(1:n), 
+                    Parent = parents,
+                    UChild = uchilds,
+                    LChild = lchilds,
+                    Length = lengths)
+  return()  
+}
