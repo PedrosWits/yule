f662c08f58c006ad014f37850740bbb97f667aec
Pedro Pinto da Silva (PGR) <ppintodasilva@gmail.com>
2016-11-01 15:28:19 +0000

    Fixed tree function

--- src/yule.R
+++ src/yule.R
@@ -3,37 +3,41 @@ getTimes = function(n=10, lambda=0.5) {
   list(length=l, time=cumsum(l))
 }
 
-buildTree = function(n=10) {
+buildTree = function(n=10, DEBUG=FALSE) {
   tree = rep(0, (2*n - 1))
   root = sample(length(tree), 1)
   tree[root] = root
   
-  for(s in 1:n) {
+  for(s in 2:n) {
     notZero = which(tree != 0)
     noChilds = notZero[!(notZero %in% tree)]
     # Root case
     if(length(noChilds) == 0) {
       noChilds = notZero
     }
-    message(cat("candidates = ", noChilds))
     # Sample behavior undesired
     if(length(noChilds) > 1) {
       parent = sample(noChilds, 1)  
     } else {
       parent = noChilds
     }
-    message(cat("Parent = ", parent))
+    
     childs = sample(which(tree==0), 2)
-    message(cat("Childs = ", childs))
     tree[childs] = parent
-    message(cat("Tree = ", tree))
-    cat("\n")
+    
+    if(DEBUG) {
+      message(cat("candidates = ", noChilds))
+      message(cat("Childs = ", childs))
+      message(cat("Parent = ", parent))
+      message(cat("Tree = ", tree))  
+    }
   }
   return(tree)
 }
 
 giveMeAYule = function(n, lambda=0.5) {
-
+  
+  
   yule = data.frame(Name   = as.character(1:n), 
                     Parent = parents,
                     UChild = uchilds,
