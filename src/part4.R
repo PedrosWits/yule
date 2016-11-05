170e1c6288adc8cc2a810b64cc20f9201e874b17
Pedro Pinto da Silva (PGR) <ppintodasilva@gmail.com>
2016-11-05 22:50:28 +0000

    Working juke cantor

--- src/part4.R
+++ src/part4.R
@@ -1,56 +1,4 @@
-jukes_cantor = function(tree, alpha, columns){
-
-  branching_Rec = function(x_v, alpha, t){  
-    p = (1/4)*(1-exp(-4*alpha*t))
-    p1 = (1/4)*(1+3*exp(-4*alpha*t))
-    child = rep(NA, length(x_v))
-    for(ip in 1:length(x_v)){
-      if(x_v[ip]==1){
-        child[ip] = sample(1:4, size=1, prob = c(p1,p,p,p))
-      }else if(x_v[ip]==2){
-        child[ip] = sample(1:4, size=1, prob = c(p,p1,p,p))
-      }else if(x_v[ip]==3){
-        child[ip] = sample(1:4, size=1, prob = c(p,p,p1,p))
-      }else{
-        child[ip] = sample(1:4, size=1, prob = c(p,p,p,p1))
-      }
-    }
-    return(child)  
-  }
-
-  # tree = rtree(4)
-  # plot(tree)
-  # nodelabels(frame="none")
-  # 
-  columns = 5
-  alpha = 0.2
-  x_v = sample(4, columns, replace=TRUE)
-  
-  num_of_leaves = length(tree$tip.label)
-  root = num_of_leaves+1
-
-  Matrix = matrix(data=NA, nrow = length(tree$edge.length)+1, ncol = columns+2)
-  Matrix[1,-c(1,2)] = x_v
-  Matrix[,1] = c(root, tree$edge[,2])
-  Matrix[1:length(tree$edge.length)+1,2] = tree$tip.label[tree$edge[,2]]
-  t = tree$edge.length
-
-  
-  for(i in 1:length(tree$edge.length)){
-    x_w1 = branching_Rec(Matrix[i,-c(1,2)], t[i], alpha=alpha)
-    Matrix[i+1,-c(1,2)] = x_w1
-  }
-  
-  alignmentMatrix = Matrix[as.numeric(Matrix[,1])<=num_of_leaves,]
-  
-  return(alignmentMatrix)
-  # mp = sapply(1:length(tree$edge.length), function(i){
-  #   branching_Rec(alignmentMatrix[i,], t[i])
-  # }, simplify = "array")
-  # 
-  # t(mp)
-}
-
+# Function to compute hamming distance
 hammingDistance = function(alignmentMatrix){
   hamDistMat = matrix(data=NA, ncol=nrow(alignmentMatrix), nrow=nrow(alignmentMatrix))
   dimnames(hamDistMat) = list(alignmentMatrix[,2],alignmentMatrix[,2])
@@ -63,54 +11,82 @@ hammingDistance = function(alignmentMatrix){
   diag(hamDistMat) = NA
   return(hamDistMat)
 }
+  
+# Function to computer Mutated Sequence
+mutateSequence = function(sequence, alpha, ts){  
+  p = (1/4) * (1 - exp(-4 * alpha * ts))
+  p1 = (1/4) * (1 + 3 * exp(-4 * alpha * ts))
+  newSeq = rep(NA, length(sequence))
+  
+  #sapply(sequence, )
+  for(ip in 1:length(sequence)){
+    if(sequence[ip] == 1){
+      newSeq[ip] = sample(1:4, size=1, prob = c(p1,p,p,p))
+    } else if(sequence[ip] == 2){
+      newSeq[ip] = sample(1:4, size=1, prob = c(p,p1,p,p))
+    } else if(sequence[ip] == 3){
+      newSeq[ip] = sample(1:4, size=1, prob = c(p,p,p1,p))
+    } else{
+      newSeq[ip] = sample(1:4, size=1, prob = c(p,p,p,p1))
+    }
+  }
+  return(newSeq)  
+}
+
+# Yet Another Jukes Cantor Model - returns Alignment Matrix
+yaJC = function(tree, alpha = 0.5, ncol = 5, ntypes = 4) {
+  n = length(tree$tip.label)
+  root = n + 1
+  rootSequence = sample(ntypes, ncol, replace=TRUE)
+  
+  alignment = matrix(data=NA, 
+                     nrow = length(tree$edge.length) + 1,
+                     ncol = ncol + 2)
+  alignment[1, -c(1,2)] = rootSequence
+  alignment[,1] = c(root, tree$edge[,2])
+  
+  treeFall = function(phylo, parent, parentSequence, alpha) {
+    # Find the childs of parent
+    childs = phylo$edge[phylo$edge[,1]==parent , 2]
+    # If no child, return
+    if(length(childs) == 0) {
+      return()
+    }
+    
+    for (child in childs) {
+      # Get the length of this edge
+      t = phylo$edge.length[phylo$edge[,2]==child]
+      print(t)
+      ## Compute child mutated sequence
+      childSeq = mutateSequence(parentSequence, alpha, t)
+      ## Act on matrix
+      alignment[alignment[,1] == child, -c(1,2)] <<- childSeq
+      ## Spawn new treeFall
+      treeFall(phylo, child, childSeq, alpha)
+    }
+  }
+  treeFall(tree, root, rootSequence, alpha)
+  alignment = alignment[as.numeric(alignment[,1]) <= n,]
+  # Get labels for second column
+  labels = tree$tip.label[tree$edge[,2]]
+  alignment[,2] = labels[!is.na(labels)]
+  return(alignment)
+}
+
+tree=rtree(10)
+plot(tree)
+nodelabels(frame="none")
+m = yaJC(tree)
+ham = hammingDistance(m)
 
 
 
+# Reset
 
-
-  tree = rtree(10)
-  plot(tree)
-  nodelabels(frame="none")
-  align = jukes_cantor(tree, alpha = 0.5, 5)
-
-
-  data("bird.orders")
-  tree = bird.orders
-  plot(tree)
-  nodelabels(frame="none")
-  
-  align = jukes_cantor(tree, alpha = 0.001, columns = 10000)
-  alignTom = jukes.cantor(tree, alpha = 0.001, col = 10000)
-  hamDist = hammingDistance(align)
-  
-  
-  which.min(hamDist)
-  hamDist[242]
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-
-  
-  
-  
+data("bird.orders")
+plot(bird.orders)  
+nodelabels(frame="none")
+m= yaJC(bird.orders, alpha=0.001, ncol=10000)
+ham = hammingDistance(m)  
+ham
   
